047c92fdfa44288c3b9df6b0fe993b18a8753417
ibaifuuuu <ibaifuentes832003@gmail.com>
2024-04-17 18:51:39 +0200

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarHorizontalComponent } from './navbar-horizontal/navbar-horizontal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HeaderComponent,
+      NavbarHorizontalComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
